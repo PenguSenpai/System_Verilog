42d53de58a527cb240e71e1e577b60e24373cbff
PenguSenpai <gavin.douglas.48@my.csun.edu>
2025-08-11 22:16:29 -0700

    Finished Always Block Learning

--- Basics/Always_Block/Always_Block.srcs/sim_1/new/Using_Always_Block.sv
+++ Basics/Always_Block/Always_Block.srcs/sim_1/new/Using_Always_Block.sv
@@ -20,7 +20,31 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module Using_Always_Block(
+module Using_Always_Block();
 
-    );
+reg clk, clk_25MHz, clk_50MHz;
+reg rst;
+
+    initial begin
+        clk = 1'b0;
+        clk_25MHz = 1'b0;
+        clk_50MHz = 1'b0;
+        rst = 1'b0;
+    end
+    //Common use of always is clock generation or toggling globle variables constantly
+    always #5 clk = ~clk;
+    
+    always #10 clk_50MHz = ~clk_50MHz;
+    
+    always #20 clk_25MHz = ~clk_25MHz;
+    initial begin
+        $dumpfile("dump.vcd");
+        $dumpvars;
+    end
+    
+    initial begin
+        #200;
+        $finish();
+    end
+        
 endmodule
